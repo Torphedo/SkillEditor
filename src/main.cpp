c8dd820957104488fe758dab1223d6270f8f3186
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-07-19 17:31:12 -0400

    Use stdio instead of fstream

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
+#include <stdio.h>
+
 #include <filesystem>
-#include <fstream>
 
 #include "imgui/imgui_backend.h"
 #include "winAPI.h"
 #include "memory-editing.h"
 
-static const char* ImGuiConfig = {
+static const char ImGuiConfig[] = {
 #include "../res/imgui-config.txt"
 };
 
-int main()
-{
+int main() {
     // Write imgui config if it doesn't already exist
-    if (!std::filesystem::exists("imgui.ini"))
-    {
-        std::ofstream config;
-        config.open("imgui.ini");
-        config << ImGuiConfig;
-        config.close();
+    if (!std::filesystem::exists("imgui.ini")) {
+        FILE* config_out = fopen("imgui.ini", "wb");
+        if (config_out != nullptr) {
+            fwrite(ImGuiConfig, sizeof(ImGuiConfig), 1, config_out);
+            fclose(config_out);
+        }
     }
 
     init_winapi();
