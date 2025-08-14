85287673157dfe0c07463d26d6875cc13704e212
Torphedo <73564623+Torphedo@users.noreply.github.com>
2025-08-14 19:14:20 -0400

    Add command-line installer flag

--- src/main.cxx
+++ src/main.cxx
@@ -3,12 +3,14 @@
 
 #include "gui_loop.hxx"
 #include "winAPI.hxx"
+#include "mods.hxx"
+#include "common/logging.h"
 
 static const char ImGuiConfig[] = {
 #include "../res/imgui-config.txt"
 };
 
-int main() {
+int main(int argc, char** argv) {
     // Write imgui config if it doesn't already exist
     if (!std::filesystem::exists("imgui.ini")) {
         FILE* config_out = fopen("imgui.ini", "wb");
@@ -18,6 +20,26 @@ int main() {
         }
     }
 
+    if (argc > 1) {
+        if (strcmp(argv[1], "--install") == 0) {
+            pd_meta p = {};
+            if (!get_process(&p)) {
+                LOG_MSG(error, "Failed to connect to Phantom Dust\n");
+                return EXIT_FAILURE;
+            }
+            update_process(&p, true);
+            flush_to_pd(p, true);
+
+            for (u32 i = 2; i < argc; i++) {
+                const char* modpath = argv[i];
+                std::string modpathStr = modpath;
+                install_mod(p, &modpathStr, 1);
+            }
+
+            flush_to_pd(p, true);
+            return  EXIT_SUCCESS;
+        }
+    }
 
     init_winapi();
     printf("Looking for Phantom Dust...\n");
